1b9ea0a5fdf94045b382d42cdbab08c187448932
Tony Dean <tdean1991@gmail.com>
2021-04-26 22:03:16 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -11,5 +11,10 @@ class profile::agent_nodes{
     ensure    => present,
     ip        => '172.18.0.3',
   }
+  
+  host {'minetest.puppet.vm':
+    ensure    => present,
+    ip        => '172.18.0.4',
+  }
  
  }
