ab63bb8b565a879ee13d4e861836a452dc22d05f
Tony Dean <tdean1991@gmail.com>
2021-04-27 00:04:04 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,7 +3,7 @@ class minecraft {
     ensure => directory,
   }
   file {'/opt/minecraft/minecraft_server.jar':
-    ensure  => file
+    ensure  => file,
     source  => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
   }
   package { 'java':  
