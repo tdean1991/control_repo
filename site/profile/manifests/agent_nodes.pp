58fcc9edd9b16dfeb00de037cd599ae750852976
Tony Dean <tdean1991@gmail.com>
2021-04-26 20:27:57 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,11 @@ class profile::agent_nodes{
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  host {'web.puppet.vm':
+    ensure  => present,
+    ip      => '172.18.0.2 ',
+ }
+ host {'db.puppet.vn': 
+  ensure    => present,
+  ip        => '172.18.0.3',
 }
