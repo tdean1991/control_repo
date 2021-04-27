68a89969b13995dd1498c12278496e091ac4e51c
Tony Dean <tdean1991@gmail.com>
2021-04-27 09:25:33 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
-  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
+class minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
   $install_dir = '/opt/minecraft'
+  ) {
   file { $install_dir: 
     ensure => directory,
   }
